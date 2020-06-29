1bd244df9197c9ff6919272efb1dca32993def85
Paul C. Bauer <paulcbauer@users.noreply.github.com>
2020-06-29 11:50:46 +0200

    Added replace braces function.

---
+++ replace_bib_braces.R
@@ -0,0 +1,21 @@
+replace_bib_braces <- function(x){
+  # Function replaces braces for bad bibtex entries
+  
+  x <- gsub("= \"", "= \\{", x)
+  x <- gsub('\",', "\\},", x)
+  
+  problematic_lines <- !str_detect(x, "\\{|\\}")
+  
+  # Add } after = 
+  x[problematic_lines] <- gsub("=  ", "= \\{", x[problematic_lines])
+  problematic_lines2 <- str_detect(x[problematic_lines], "= \\{")
+  x[problematic_lines][problematic_lines2] <- gsub(",$", "\\},", x[problematic_lines][problematic_lines2])
+  
+  # Add } before string end
+  problematic_lines3 <- str_detect(x[problematic_lines], "= \\{") & !str_detect(x[problematic_lines], "\\}")
+  x[problematic_lines][problematic_lines3] <- paste(x[problematic_lines][problematic_lines3], "}", sep="")
+  
+  x <- gsub("\"", "\\}", x)
+  x
+  
+}  
